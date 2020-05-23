82e8e2549fda02e4a61ef5eeec4955c52e8b7166
uedananaka <seven7u-ap-1126os@ezweb.ne.jp>
2020-05-23 15:21:54 +0900

    課題0522分

---
+++ php01/data/date.php
@@ -0,0 +1,5 @@
+<?php?
+$d = date("Y-m-d H:i:s");//YとHは大文字
+echo $d;
+
+>
--- php01/post_confirm.php
+++ php01/post_confirm.php
@@ -1,7 +1,20 @@
 <?php
+function h($value){
+    return htmlspecialchars($value,ENT_QUOTES);
+}
+
 $flg = 0;
 $name = $_POST["name"];
 $mail = $_POST["mail"];
+
+//File書き込み
+//課題は、名前とメールの変数をカンマ区切りの文字列にしてファイルに書き込めるようにする
+
+$file = fopen("data/data.txt","a");	// ファイル読み込み
+fwrite($file,"$name.$mail\n");
+fclose($file);
+
+
 if($name == ""){
     $name = "未入力です";
     $flg = 1;
@@ -18,8 +31,8 @@ if($mail == ""){
 <title>POST（受信）</title>
 </head>
 <body>
-お名前：<?php echo $name; ?>
-EMAIL：<?php echo $mail; ?>
+お名前：<?php echo h($name); ?>
+EMAIL：<?php echo h($mail); ?>
 
 <?php 
     if($flg == 0){
